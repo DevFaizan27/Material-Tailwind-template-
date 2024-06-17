6f0ba687e0f0fe8e822e730f35818747e398cc59
Mohd faizan Hussain <faizanhussain78612@gmail.com>
2024-06-17 13:41:26 +0530

    hello

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import { Button } from '@material-tailwind/react'
+import React from 'react'
+
+const App = () => {
+  return (
+    <div className='bg-red-900'>
+      <Button >Click me</Button>
+    </div>
+  )
+}
+
+export default App
